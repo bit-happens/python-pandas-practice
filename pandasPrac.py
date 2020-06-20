1a649306cbdff0c2121dccf900e34cdf2a5473d4
ET <Earnestwt3@gmail.com>
2020-06-20 11:36:44 -0500

    added code to test_data.csv and pandasPrac.py files

--- pandasPrac.py
+++ pandasPrac.py
@@ -5,8 +5,20 @@ data = {
 	'apples': [3,2,0,1],
 	'oranges': [0,3,7,2]
 }
-
+"""
 #pass the dictionary to the pandas DataFrame constructor
 purchases = pd.DataFrame(data)
-
 print(purchases)
+"""
+"""
+#Use customers names as indexes
+purchases = pd.DataFrame(data, index=['June', 'Robert', 'Lily', 'David'])
+print(purchases)
+"""
+#Read the data from a csv file
+df = pd.read_csv('test_data.csv')
+print(df)
+
+#Remove auto generated indexes
+df = pd.read_csv('test_data.csv',index_col=0)
+print(df)
